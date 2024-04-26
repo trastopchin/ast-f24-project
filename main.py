2af2ef2448452ff25827dc37f11d248ae5ce76ce
Tal Rastopchin <trastopchin@gmail.com>
2024-04-26 22:59:20 +0200

    completely restructure and reimplement the MPSFile classes. the MPSFile class only keeps track of the model and lazy computes the objectiv evalue. an MPSMutation represents an object that takes a list of MPSFiles and produces a new MPSFile. it has a method that represents the metamorphic relation between the input files and the output files. an MPSMetamorphicRelation represents the triple of input files, output file, and mutation that follow a metamorphic relation.

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+from typing import List
+
+from mps_fuzzer import MPSFile, MPSMutation, MPSMetamorphicRelation
+from mps_fuzzer import TranslateObjective, ScaleObjective
+
+
+def set_gurobi_license(filepath: str):
+    """Set the gurobi license environment variable."""
+    os.environ['GRB_LICENSE_FILE'] = filepath
+
+
+if __name__ == '__main__':
+    set_gurobi_license('./gurobi.lic')
+
+    # Read the seed files
+    seed_files = MPSFile.read_files('./input')
+    generated_files = list[tuple[MPSFile, MPSMetamorphicRelation]]()
+
+    # Iteratiely apply the mutations
+    current_files = seed_files
+    next_files = list[MPSFile]()
+    depth = 4
+
+    for i in range(depth):
+        # Translate Objective
+        for input_file in current_files:
+            translate_objective = TranslateObjective()
+            output_file, relation = translate_objective.mutate([input_file])
+            holds, relation_str = relation.check(debug=True)
+            print(output_file)
+            next_files.append(output_file)
+            generated_files.append(output_file)
+
+        # Scale Objective
+        for input_file in current_files:
+            scale_objective = ScaleObjective()
+            output_file, relation = scale_objective.mutate([input_file])
+            holds, relation_str = relation.check(debug=True)
+            print(output_file)
+            next_files.append(output_file)
+            generated_files.append(output_file)
+
+        current_files = next_files.copy()
+        next_files.clear()
--- mps_fuzzer.py
+++ mps_fuzzer.py
@@ -1,250 +1,234 @@
+# Future imports
 from __future__ import annotations
-from typing import Optional, List
-import os
-import gurobipy as gp
-import numpy as np
+
+# Standard library imports
+from typing import List, Optional
+from pathlib import Path
 import contextlib
 import io
-import queue
 import random
+from copy import deepcopy
 
-# Forward references
-
-
-class MPS:
-    pass
-
-
-class Mutation:
-    pass
+# Third-party imports
+import numpy as np
+import gurobipy as gp
 
 
-def set_gurobi_license(filepath: str):
-    """Set the gurobi license environment variable."""
-    os.environ['GRB_LICENSE_FILE'] = filepath
+class MPSFile:
+    """MPSFile class"""
+    filename: str
+    model: gp.Model
+    _obj_val: Optional[float]
 
-
-class MPS:
-    """MPS class"""
-    model: Optional[gp.Model]
-    obj_val: Optional[float]
-    solution: Optional[np.ndarray]
-    mutations: List[Mutation]
-
-    def __init__(self):
-        """Empty constructor."""
-        self.model = None
-        self.obj_val = None
-        self.mutations = []
-
-    def optimize(self):
-        """Optimize the program and save the objective value"""
-
-        if self.model == None:
-            print('Model not initialized.')
-            return
-
-        self.model.setParam('OutputFlag', 0)
-        self.model.optimize()
-        if self.model.Status == gp.GRB.OPTIMAL:
-            self.obj_val = float(self.model.ObjVal)
-            self.solution = np.zeros(self.model.NumVars)
-            for i in range(self.model.NumVars):
-                self.solution[i] = self.model.getVars()[i].X
-        else:
-            self.obj_val = None
-            self.solution = None
+    def __init__(self, filename: Path, model: gp.Model):
+        self.filename = filename
+        self.model = model
+        self._obj_val = None
 
     @ staticmethod
-    def from_model(model: gp.Model):
-        """Create an MPS object from a gp.Model."""
-        mps = MPS()
-        mps.model = model.copy()
-        mps.optimize()
-        return mps
-
-    @ staticmethod
-    def from_filepath(filepath: str):
-        """Create an MPS object from a filepath"""
-
-        # Read the mps file
+    def from_filepath(filepath: str) -> MPSFile:
+        """Create an MPSFile from a filepath."""
+        filename = Path(filepath).name
         model = None
         with contextlib.redirect_stdout(io.StringIO()):
             model = gp.read(filepath)
 
-        return MPS.from_model(model)
+        return MPSFile(filename, model)
 
-    def copy(self):
-        """Copy an MPS object"""
-        mps = MPS()
-        mps.model = self.model.copy()
-        mps.obj_val = self.obj_val
-        mps.solution = self.solution.copy()
-        mps.mutations = self.mutations.copy()
-        return mps
+    @staticmethod
+    def read_files(dir: str) -> List[MPSFile]:
+        """Read a list of mps files from a directory."""
+        mps_files = []
+        for file in Path(dir).iterdir():
+            if file.is_file():
+                mps_file = MPSFile.from_filepath(str(file))
+                mps_files.append(mps_file)
+        return mps_files
+
+    @staticmethod
+    def write_files(dir: str, mps_files: List[MPSFile]):
+        """Write a list of mps files to a directory."""
+        for mps_file in mps_files:
+            output_path = Path(dir) / mps_file.filename
+            mps_file.model.write(str(output_path))
+
+    def copy(self) -> MPSFile:
+        """Copy an MPSFile object"""
+        self.model.update()
+        return MPSFile(self.filename, self.model.copy())
+
+    def append_to_filename(self, to_append: str):
+        """Append a string to the filename."""
+        stem_and_extension = self.filename.split('.', 1)
+        stem = stem_and_extension[0]
+        extension = stem_and_extension[1]
+        self.filename = stem + to_append + '.' + extension
+
+    def optimize(self):
+        """Optimize the program and save the objective value."""
+        # TODO: do we need to check that self.model.Status == gp.GRB.OPTIMAL?
+        self.model.setParam('OutputFlag', 0)
+        self.model.update()
+        self.model.optimize()
+        self._obj_val = self.model.ObjVal
+
+    def obj_val(self):
+        """Lazy compute the objective value."""
+        if self._obj_val == None:
+            self.optimize()
+        return self._obj_val
 
     def __repr__(self) -> str:
-        """Print the number of constraints, the number of variables, and the number of mutations"""
-        return f"(Num Mutations: {len(self.mutations)}, Mutations: {str(self.mutations)})"
-
-    @ staticmethod
-    def _linexpr_to_ndarray(linexpr: gp.LinExpr) -> np.ndarray:
-        """Convert a gp.LinExpr to a np.ndarray"""
-        n = linexpr.size()
-        ndarray = np.zeros(n + 1)
-        ndarray[0] = linexpr.getConstant()
-        for i in range(n):
-            ndarray[i+1] = linexpr.getCoeff(i)
-        return ndarray
-
-    @ staticmethod
-    def _ndarray_to_linexpr(model: gp.Model, ndarray: np.ndarray) -> gp.LinExpr:
-        """Convert a np.ndarray to a gp.LinExpr"""
-        linexpr = gp.LinExpr()
-        linexpr.addConstant(ndarray[0])
-        linexpr.addTerms(ndarray[1:], model.getVars())
-        return linexpr
+        """Return the filename."""
+        return self.filename
 
 
-class Mutation:
-    """Mutation base class."""
+def _linexpr_to_ndarray(linexpr: gp.LinExpr) -> tuple[float, np.ndarray]:
+    """Convert a gp.LinExpr to a float constant and np.ndarray of coefficients."""
+    constant = linexpr.getConstant()
+    n = linexpr.size()
+    coefficients = np.zeros(n)
+    for i in range(n):
+        coefficients[i] = linexpr.getCoeff(i)
+    return constant, coefficients
 
-    def mutate(self, mps: MPS):
+
+def _ndarray_to_linexpr(model: gp.Model, constant: float, coefficients: np.ndarray) -> gp.LinExpr:
+    """Convert a float constant and np.ndarray of coefficents to a gp.LinExpr."""
+    linexpr = gp.LinExpr()
+    linexpr.addConstant(constant)
+    linexpr.addTerms(coefficients, model.getVars())
+    return linexpr
+
+
+class MPSMutation:
+    """Represents a mutation of an MPSFile."""
+
+    def mutate(self, input: List[MPSFile]) -> tuple[MPSFile, Optional[MPSMetamorphicRelation]]:
+        pass
+
+    def metamorphic_relation(self, input_files: List[MPSFile], output_file: MPSFile) -> tuple[bool, str]:
         pass
 
 
-class ScaleObjective(Mutation):
-    """Scale objective mutation"""
+class MPSMetamorphicRelation:
+    """Represents a metamorphic relation between a list of input MPSFiles and an output MPSFile."""
+    input_files: List[MPSFile]
+    output_file: MPSFile
+    mutation: MPSMutation
 
-    def __init__(self, scale: int):
-        self.scale = np.random.randint(-scale, scale)
+    def __init__(self, input_files: List[MPSFile], output_file: MPSFile, mutation: MPSMutation):
+        self.input_files = input_files
+        self.output_file = output_file
+        self.mutation = mutation
 
-    def __repr__(self):
-        return f"{self.scale} * objective"
+    def check(self, debug=False) -> tuple[bool, str]:
+        """Check that the metamorphic relation holds."""
+        # Check if the relation holds and get the relation string
+        holds, relation_str = self.mutation.metamorphic_relation(
+            self.input_files, self.output_file)
 
-    def mutate(self, mps: MPS):
-        # Perform the mutation
-        mps_new = mps.copy()
-        mps_new.mutations.append(self)
-        objective_linexpr = mps_new.model.getObjective()
-        objective_ndarray = MPS._linexpr_to_ndarray(objective_linexpr)
-        objective_ndarray_new = self.scale * objective_ndarray
-        objective_linexpr_new = MPS._ndarray_to_linexpr(
-            mps_new.model, objective_ndarray_new)
-        mps_new.model.setObjective(objective_linexpr_new)
-        mps_new.optimize()
+        # Print debug information if the relatino doesn't hold
+        if not holds and debug:
+            print(self.mutation)
+            print(f"\tinput: {self.input_files}")
+            print(f"\toutput: {self.output_file}")
+            print(f"\trelation: {relation_str}")
 
-        # Check the metamorphic relationship
-        objective_relation = mps_new.obj_val == self.scale * mps.obj_val
-        if not objective_relation:
-            print("Objective relation doesn't hold!")
-            print(f"Expected objective: {self.scale * mps.obj_val}")
-            print(f"Actual objective: {mps_new.obj_val}")
-        # solution_relation = np.array_equal(mps_new.solution, mps.solution)
-        # if not solution_relation:
-        #     print("Solution relation doesn't hold!")
-
-        return mps_new
+        return holds, relation_str
 
 
-class TranslateObjective(Mutation):
-    """Translate objective mutation"""
+class TranslateObjective(MPSMutation):
+    """Translates the MIP objective function."""
+    translation: int
 
-    def __init__(self, translation: int):
-        self.translation = np.random.randint(-translation, translation)
-
-    def __repr__(self):
-        return f"{self.translation} + objective"
-
-    def mutate(self, mps: MPS):
-
-        # Perform the mutation
-        mps_new = mps.copy()
-        mps_new.mutations.append(self)
-        objective_linexpr = mps_new.model.getObjective()
-        objective_ndarray = MPS._linexpr_to_ndarray(objective_linexpr)
-        objective_ndarray_new = objective_ndarray.copy()
-        objective_ndarray_new[0] += self.translation
-        objective_linexpr_new = MPS._ndarray_to_linexpr(
-            mps_new.model, objective_ndarray_new)
-        mps_new.model.setObjective(objective_linexpr_new)
-        mps_new.optimize()
-
-        # Check the metamorphic relationship
-        objective_relation = mps_new.obj_val == self.translation + mps.obj_val
-        if not objective_relation:
-            print("Objective relation doesn't hold!")
-            print(f"Expected objective: {self.translation + mps.obj_val}")
-            print(f"Actual objective: {mps_new.obj_val}")
-        # solution_relation = np.array_equal(mps_new.solution, mps.solution)
-        # if not solution_relation:
-        #     print("Solution relation doesn't hold!")
-        return mps_new
-
-
-class TranslateVariables(Mutation):
-    """Translate variables mutation"""
-
-    def __init__(self, translation: int):
-        self.initialzed = False
+    def __init__(self, translation=None):
+        # TODO: how do we sample objective translations?
+        if translation is None:
+            translation = np.random.randint(-1000, 1000)
+            # TODO: this breaks the metamorphic relation
+            # translation = np.random.randint(-100000, 100000)
         self.translation = translation
 
-    def __repr__(self):
-        return f"{str(self.translation)} + variables"
+    def mutate(self, input_files: List[MPSFile]) -> tuple[MPSFile, Optional[MPSMetamorphicRelation]]:
+        # Assert there is one input file
+        assert (len(input_files) == 1)
+        input_file = input_files[0]
 
-    def mutate(self, mps: MPS):
-        if not self.initialized:
-            self.translation = np.random.randint(
-                -self.translation, self.translation, shape=(mps.model.NumVars,))
-            self.initialzed = True
+        # Copy the MPSFile
+        output_file = input_file.copy()
+        output_file.append_to_filename('_TO')
 
-        # Perform the mutation
-        mps_new = mps.copy()
-        mps_new.mutations.append(self)
-        objective_linexpr = mps_new.model.getObjective()
-        objective_ndarray = MPS._linexpr_to_ndarray(objective_linexpr)
-        objective_ndarray_new = objective_ndarray.copy()
-        objective_ndarray_new += self.translation
-        objective_linexpr_new = MPS._ndarray_to_linexpr(
-            mps_new.model, objective_ndarray_new)
-        mps_new.model.setObjective(objective_linexpr_new)
-        mps_new.optimize()
+        # Translate the objective
+        objective_linexpr = output_file.model.getObjective()
+        constant, coefficients = _linexpr_to_ndarray(objective_linexpr)
+        objective_linexpr_new = _ndarray_to_linexpr(
+            output_file.model, constant + self.translation, coefficients)
+        output_file.model.setObjective(objective_linexpr_new)
 
-        # Check the metamorphic relationship
-        objective_relation = mps_new.obj_val == mps.obj_val
-        if not objective_relation:
-            print("Objective relation doesn't hold!")
-            print(f"Expected objective: {mps.obj_val}")
-            print(f"Actual objective: {mps_new.obj_val}")
-        # solution_relation = np.array_equal(mps_new.solution, mps.solution)
-        # if not solution_relation:
-        #     print("Solution relation doesn't hold!")
-        return mps_new
+        # Create the metamorphic relation
+        metamorphic_relation = MPSMetamorphicRelation(
+            input_files, output_file, self)
+
+        # Return the output file and the metamorphic relation
+        return output_file, metamorphic_relation
+
+    def metamorphic_relation(self, input_files: List[MPSFile], output_file: MPSFile) -> tuple[bool, str]:
+        """Metamorphic relation."""
+        # Assert there is one input file
+        assert (len(input_files) == 1)
+        input_file = input_files[0]
+
+        input_obj = input_file.obj_val()
+        output_obj = output_file.obj_val()
+        relation = self.translation + input_obj == output_obj
+        relation_str = f"{self.translation} + {input_obj}  == {output_obj}"
+
+        return relation, relation_str
 
 
-if __name__ == '__main__':
-    # Gurobi license path
-    set_gurobi_license('./gurobi.lic')
+class ScaleObjective(MPSMutation):
+    """Scale the MIP objective function."""
+    scale: int
 
-    # Seed filepath
-    filepath = './10teams.mps.gz'
-    seed = MPS.from_filepath(filepath)
+    def __init__(self, scale: int = None):
+        # TODO: how do we sample objective scales?
+        if scale is None:
+            scale = np.random.randint(1, 10)
+        self.scale = scale
 
-    # Depth first search
-    depth = 10
-    generated = []
-    queue = queue.Queue()
-    queue.put(seed)
+    def mutate(self, input_files: List[MPSFile]) -> tuple[MPSFile, Optional[MPSMetamorphicRelation]]:
+        # Assert there is one input file
+        assert (len(input_files) == 1)
+        input_file = input_files[0]
 
-    while not queue.empty():
-        current = queue.get()
+        # Copy the MPSFile
+        output_file = input_file.copy()
+        output_file.append_to_filename('_TS')
 
-        if len(current.mutations) >= depth:
-            continue
+        # Translate the objective
+        objective_linexpr = output_file.model.getObjective()
+        constant, coefficients = _linexpr_to_ndarray(objective_linexpr)
+        objective_linexpr_new = _ndarray_to_linexpr(
+            output_file.model, self.scale * constant, self.scale * coefficients)
+        output_file.model.setObjective(objective_linexpr_new)
 
-        mutations = [ScaleObjective(10), TranslateObjective(
-            1000), TranslateObjective(1000)]
-        mutation = random.choice(mutations)
-        mutated = mutation.mutate(current)
-        print(mutated)
-        queue.put(mutated)
+        # Create the metamorphic relation
+        metamorphic_relation = MPSMetamorphicRelation(
+            input_files, output_file, self)
+
+        # Return the output file and the metamorphic relation
+        return output_file, metamorphic_relation
+
+    def metamorphic_relation(self, input_files: List[MPSFile], output_file: MPSFile) -> tuple[bool, str]:
+        """Metamorphic relation."""
+        # Assert there is one input file
+        assert (len(input_files) == 1)
+        input_file = input_files[0]
+
+        input_obj = input_file.obj_val()
+        output_obj = output_file.obj_val()
+        relation = self.scale * input_obj == output_obj
+        relation_str = f"{self.scale} * {input_obj} == {output_obj}"
+
+        return relation, relation_str
