5ae4255712aead331385b60cfc31ea2a18497740
mnahinkhan <mnk1@alumni.cmu.edu>
2024-04-27 15:58:39 +0000

    Add Gurobi solver to mps fuzzing classes (untested)

--- mps_fuzzer.py
+++ mps_fuzzer.py
@@ -8,32 +8,108 @@ import contextlib
 import io
 import random
 from copy import deepcopy
+import itertools
 
 # Third-party imports
 import numpy as np
 import gurobipy as gp
+import cplex as cp
 
+
+# Enumerate the possible solver types
+class Solver(str):
+    GUROBI = "gurobi"
+    CPLEX = "cplex"
+
+class CplexSolveError(Exception):
+    """Custom exception for Cplex solve errors."""
+
+# def copy_cplex_model(old_cpx: cp.Cplex) -> cp.Cplex:
+#     """Clone a cplex model object"""
+#     # Check if this works...
+#     new_cpx = cp.Cplex()
+#     new_cpx.copylp(numcols=old_cpx.variables.get_num(),
+#                numrows=old_cpx.linear_constraints.get_num(),
+#                objsense=old_cpx.objective.get_sense(),
+#                obj=old_cpx.objective.get_linear(),
+#                rhs=old_cpx.linear_constraints.get_rhs(),
+#                senses=old_cpx.linear_constraints.get_senses(),
+#                matbeg=old_cpx.linear_constraints.get_rows()[0],
+#                matcnt=old_cpx.linear_constraints.get_rows()[1],
+#                matind=old_cpx.linear_constraints.get_rows()[2],
+#                matval=old_cpx.linear_constraints.get_coefficients(),
+#                lb=old_cpx.variables.get_lower_bounds(),
+#                ub=old_cpx.variables.get_upper_bounds())
+#     return new_cpx
+
+
+def solve_cplex_model(cpx: cp.Cplex, debug=False) -> float:
+    """
+    Solve a CPLEX model and return the objective value.
+    """
+    # Inspired by: https://github.com/cswaroop/cplex-samples/blob/master/mipex2.py
+
+    cpx.solve()
+
+    # solution.get_status() returns an integer code
+    status = cpx.solution.get_status()
+    if debug:
+        print(cpx.solution.status[status])
+
+    if status == cpx.solution.status.unbounded:
+        raise(CplexSolveError("Model is unbounded"))
+    if status == cpx.solution.status.infeasible:
+        raise(CplexSolveError("Model is infeasible"))
+    if status == cpx.solution.status.infeasible_or_unbounded:
+        raise(CplexSolveError("Model is infeasible or unbounded"))
+
+    s_method = cpx.solution.get_method()
+    s_type   = cpx.solution.get_solution_type()
+
+    if debug:
+        print("Solution status = " , status, ":")
+        # the following line prints the status as a string
+        print(cpx.solution.status[status])
+    
+    if s_type == cpx.solution.type.none:
+        raise(CplexSolveError("No solution available"))
+
+    if debug:
+        print("Objective value = " , cpx.solution.get_objective_value())
+
+        x = cpx.solution.get_values(0, cpx.variables.get_num()-1)
+        # because we're querying the entire solution vector,
+        # x = c.solution.get_values()
+        # would have the same effect
+        for j in range(cpx.variables.get_num()):
+            print("Column %d: Value = %17.10g" % (j, x[j]))
+
+    return cpx.solution.get_objective_value()
 
 class MPSFile:
     """MPSFile class"""
     filename: str
-    model: gp.Model
-    _obj_val: Optional[float]
+    gurobi_model: gp.Model
+    cplex_model: cp.Cplex
+    _obj_val_gurobi: Optional[float]
+    _obj_val_cplex: Optional[float]
 
-    def __init__(self, filename: Path, model: gp.Model):
+    def __init__(self, filename: Path, gurobi_model: gp.Model, cplex_model: Optional[cp.Cplex]):
         self.filename = filename
-        self.model = model
-        self._obj_val = None
+        self.gurobi_model = gurobi_model
+        self.cplex_model = cplex_model
+        self._obj_val_gurobi = None
+        self._obj_val_cplex = None
 
     @ staticmethod
     def from_filepath(filepath: str) -> MPSFile:
         """Create an MPSFile from a filepath."""
         filename = Path(filepath).name
-        model = None
         with contextlib.redirect_stdout(io.StringIO()):
-            model = gp.read(filepath)
+            gurobi_model = gp.read(filepath)
+            cplex_model = cp.Cplex(filepath)
 
-        return MPSFile(filename, model)
+        return MPSFile(filename, gurobi_model, cplex_model)
 
     @staticmethod
     def read_files(dir: str) -> Generator[MPSFile]:
@@ -52,8 +128,42 @@ class MPSFile:
 
     def copy(self) -> MPSFile:
         """Copy an MPSFile object"""
-        self.model.update()
-        return MPSFile(self.filename, self.model.copy())
+        self.gurobi_model.update()
+        return MPSFile(
+            self.filename,
+            self.gurobi_model.copy(),
+            # copy_cplex_model(self.cplex_model)
+            None
+        )
+    
+    def refresh_cplex_from_gurobi(self):
+        """Refresh the CPLEX model from the Gurobi model."""
+        self.cplex_model = cp.Cplex()
+
+        # TODO: Maybe compare the approaches
+
+        # First approach: copy the Gurobi model to CPLEX by extracting params
+        # self.cplex_model.copylp(numcols=self.gurobi_model.NumVars,
+        #                         numrows=self.gurobi_model.NumConstrs,
+        #                         objsense=self.gurobi_model.getAttr(gp.GRB.Attr.ModelSense),
+        #                         obj=self.gurobi_model.getAttr(gp.GRB.Attr.Obj),
+        #                         rhs=self.gurobi_model.getAttr(gp.GRB.Attr.RHS),
+        #                         senses=self.gurobi_model.getAttr(gp.GRB.Attr.Sense),
+        #                         matbeg=self.gurobi_model.getAttr(gp.GRB.Attr.VBasis),
+        #                         matcnt=self.gurobi_model.getAttr(gp.GRB.Attr.VBasis),
+        #                         matind=self.gurobi_model.getAttr(gp.GRB.Attr.VBasis),
+        #                         matval=self.gurobi_model.getAttr(gp.GRB.Attr.VBasis),
+        #                         lb=self.gurobi_model.getAttr(gp.GRB.Attr.LB),
+        #                         ub=self.gurobi_model.getAttr(gp.GRB.Attr.UB))
+
+        # Second approach: copy the Gurobi model to CPLEX by reading and writing to a fake MPS file
+        with contextlib.redirect_stdout(io.StringIO()):
+            rand_name = str(random.randint(0, 1000000))
+            self.gurobi_model.write(f'temp{rand_name}.mps')
+            self.cplex_model.read(f'temp{rand_name}.mps')
+            Path(f'temp{rand_name}.mps').unlink()
+
+
 
     def append_to_filename(self, to_append: str):
         """Append a string to the filename."""
@@ -62,19 +172,31 @@ class MPSFile:
         extension = stem_and_extension[1]
         self.filename = stem + to_append + '.' + extension
 
-    def optimize(self):
+    def optimize(self, debug=False):
         """Optimize the program and save the objective value."""
         # TODO: do we need to check that self.model.Status == gp.GRB.OPTIMAL?
-        self.model.setParam('OutputFlag', 0)
-        self.model.update()
-        self.model.optimize()
-        self._obj_val = self.model.ObjVal
+        if not debug:
+            self.gurobi_model.setParam('OutputFlag', 0)
+            self.cplex_model.set_results_stream(None)
 
-    def obj_val(self):
-        """Lazy compute the objective value."""
-        if self._obj_val == None:
+        self.gurobi_model.update()
+        self.gurobi_model.optimize()
+        self._obj_val_gurobi = self.gurobi_model.ObjVal
+
+        self._obj_val_cplex = solve_cplex_model(self.cplex_model, debug=debug)
+
+
+    def obj_val_gurobi(self):
+        """Lazy compute the Gurobi objective value."""
+        if self._obj_val_gurobi == None:
             self.optimize()
-        return self._obj_val
+        return self._obj_val_gurobi
+
+    def obj_val_cplex(self):
+        """Lazy compute the CPLEX objective value."""
+        if self._obj_val_cplex == None:
+            self.optimize()
+        return self._obj_val_cplex
 
     def __repr__(self) -> str:
         """Return the filename."""
@@ -126,15 +248,24 @@ class MPSMetamorphicRelation:
     def check(self, debug=False) -> tuple[bool, str]:
         """Check that the metamorphic relation holds."""
         # Check if the relation holds and get the relation string
-        holds, relation_str = self.mutation.metamorphic_relation(
-            self.input_files, self.output_file)
+        input_solver_types = [Solver.GUROBI, Solver.CPLEX]
+        output_solver_types = [Solver.GUROBI, Solver.CPLEX]
+        holds_all = True
+        for input_solver_type, output_solver_type in itertools.product(input_solver_types, output_solver_types):
+            holds, relation_str = self.mutation.metamorphic_relation(
+                self.input_files, self.output_file, input_solver_type, output_solver_type
+            )
 
-        # Print debug information if the relatino doesn't hold
-        if not holds and debug:
-            print(self.mutation)
-            print(f"\tinput: {self.input_files}")
-            print(f"\toutput: {self.output_file}")
-            print(f"\trelation: {relation_str}")
+            # Print debug information if the relation doesn't hold
+            if not holds and debug:
+                print(self.mutation)
+                print(f"\tinput: {self.input_files}")
+                print(f"\toutput: {self.output_file}")
+                print(f"\trelation: {relation_str}")
+                print(f"\tinput_solver_type: {input_solver_type}")
+                print(f"\toutput_solver_type: {output_solver_type}")
+            
+            holds_all = holds_all and holds
 
         return holds, relation_str
 
@@ -161,12 +292,14 @@ class TranslateObjective(MPSMutation):
         output_file.append_to_filename('_TO')
 
         # Translate the objective
-        objective_linexpr = output_file.model.getObjective()
+        objective_linexpr = output_file.gurobi_model.getObjective()
         constant, coefficients = _linexpr_to_ndarray(
-            output_file.model, objective_linexpr)
+            output_file.gurobi_model, objective_linexpr)
         objective_linexpr_new = _ndarray_to_linexpr(
-            output_file.model, constant + self.translation, coefficients)
-        output_file.model.setObjective(objective_linexpr_new)
+            output_file.gurobi_model, constant + self.translation, coefficients)
+        output_file.gurobi_model.setObjective(objective_linexpr_new)
+
+        output_file.refresh_cplex_from_gurobi()
 
         # Create the metamorphic relation
         metamorphic_relation = MPSMetamorphicRelation(
@@ -175,14 +308,20 @@ class TranslateObjective(MPSMutation):
         # Return the output file and the metamorphic relation
         return output_file, metamorphic_relation
 
-    def metamorphic_relation(self, input_files: List[MPSFile], output_file: MPSFile) -> tuple[bool, str]:
+    def metamorphic_relation(
+            self,
+            input_files: List[MPSFile],
+            output_file: MPSFile,
+            input_solver_type: Solver = Solver.GUROBI,
+            output_solver_type: Solver = Solver.GUROBI
+    ) -> tuple[bool, str]:
         """Metamorphic relation."""
         # Assert there is one input file
         assert (len(input_files) == 1)
         input_file = input_files[0]
 
-        input_obj = input_file.obj_val()
-        output_obj = output_file.obj_val()
+        input_obj = input_file.obj_val_gurobi() if input_solver_type == Solver.GUROBI else input_file.obj_val_cplex()
+        output_obj = output_file.obj_val_gurobi() if output_solver_type == Solver.GUROBI else output_file.obj_val_cplex()
         relation = self.translation + input_obj == output_obj
         relation_str = f"{self.translation} + {input_obj}  == {output_obj}"
 
@@ -216,6 +355,8 @@ class ScaleObjective(MPSMutation):
             output_file.model, self.scale * constant, self.scale * coefficients)
         output_file.model.setObjective(objective_linexpr_new)
 
+        output_file.refresh_cplex_from_gurobi()
+
         # Create the metamorphic relation
         metamorphic_relation = MPSMetamorphicRelation(
             input_files, output_file, self)
@@ -223,14 +364,20 @@ class ScaleObjective(MPSMutation):
         # Return the output file and the metamorphic relation
         return output_file, metamorphic_relation
 
-    def metamorphic_relation(self, input_files: List[MPSFile], output_file: MPSFile) -> tuple[bool, str]:
+    def metamorphic_relation(
+            self,
+            input_files: List[MPSFile],
+            output_file: MPSFile,
+            input_solver_type: Solver = Solver.GUROBI,
+            output_solver_type: Solver = Solver.GUROBI
+    ) -> tuple[bool, str]:
         """Metamorphic relation."""
         # Assert there is one input file
         assert (len(input_files) == 1)
         input_file = input_files[0]
 
-        input_obj = input_file.obj_val()
-        output_obj = output_file.obj_val()
+        input_obj = input_file.obj_val() if input_solver_type == Solver.GUROBI else input_file.obj_val_cplex()
+        output_obj = output_file.obj_val() if output_solver_type == Solver.GUROBI else output_file.obj_val_cplex()
         relation = self.scale * input_obj == output_obj
         relation_str = f"{self.scale} * {input_obj} == {output_obj}"
 
